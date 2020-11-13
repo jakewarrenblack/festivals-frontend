e16412f0ff461402a7179ad6d5c3346a740367fe
Jake Black <jakewarrenblack01@gmail.com>
2020-11-13 22:02:45 +0000

    Redesign homepage and add javascript fade effect

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 
+    
+$(window).on("load",function() {
 
-window.addEventListener(
-  "scroll",
-  () => {
-    document.body.style.setProperty(
-      "--scroll",
-      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-    );
-  },
-  false
-);
-
+  if($(window).width() >= 1024){
+    console.log($(window).width())
+  // alert('found')
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(300,1);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+}
+});
