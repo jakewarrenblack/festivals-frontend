e0b3b90e6a6c59d6fe759afbc69380c4f5a925a4
Jake Black <jakewarrenblack01@gmail.com>
2020-11-15 20:04:01 +0000

    Update homepage and ui kit. Add sign up form w/language icons.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,44 @@
 
 
-$(window).on("load",function() {
+/*Not using window.onLoad because this script is needed at load time!*/
+/*I don't want to wait for images to load before having this script ready to go.*/
+$(document).ready(function() {
+
+
+scrollDownBtn = document.getElementById("scrollDown");
+ mainImg = document.getElementById("mainImg");
+//Find our button
+backTopBtn = document.getElementById("backTop");
+window.onscroll = function () {
+    backTop()
+    scrollDown();
+};
+
+
+/*When we scroll below the hero image, hide the floating arrow*/
+function scrollDown(){
+  if (document.body.scrollTop > (mainImg.offsetHeight - mainImg.offsetHeight/2) || document.documentElement.scrollTop > (mainImg.offsetHeight- mainImg.offsetHeight/2)) {
+    scrollDownBtn.style.display = "none";
+} else {
+  scrollDownBtn.style.display = "block";
+}
+}
+
+function backTop() {
+    if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight ) {
+        backTopBtn.style.display = "block";
+    } else {
+        backTopBtn.style.display = "none";
+    }
+}
+
   var collapseBtn = document.getElementById("collapseButton");
   var collapsedElems = document.getElementsByClassName("collapse");
 
   /*Initalising to 0*/
 var counter = 0;
 
+/*Change the innerHTML of the 'see more' button when clicked*/
   function changeHTML() {
     counter++;
     if(counter % 2 != 0){
@@ -38,10 +70,10 @@ if($(window).width() >= 1024){
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+      if (objectBottom-500 < windowBottom) { //object comes into view (scrolling down)
         if ($(this).css("opacity")==0) {$(this).fadeTo(300,1);}
       }
     });
-  }).scroll(); //invoke scroll-handler on page-load
+  }).scroll(); //run scroll-handler on page-load
 }
 });
